ed9b58dc530e06d14348e0196a579c39ce6f9b60
Brayan Sudario <112755263+brayan-sudario@users.noreply.github.com>
2026-08-04 17:48:39 +0100

    Contact page (#8)
    * 1st Commit
    
    * Added Contact Page html,css and js files
    
    * Created Hero Section
    
    * Fixed layout of elements
    
    * Added screen size responsiveness
    
    * Added button animation
    
    * Added form validation logic
    
    * Implemented Success Modal
    
    * Revert "Merge branch 'properties-page' of https://github.com/CarterrRyan/Com109-group8 into ContactPage"
    
    This reverts commit 91355202fe2c19c618b562ba65e2e89e766b9fa9, reversing
    changes made to 13042efe99ff13facd2409cb79210445198d24bb.
    
    * Restore properties page files
    
    * Updated styling for uniformity
    
    * Saved form input values to sessionStorage

---
+++ js/ContactPage.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+  const $form = $("#contact-form");
+  const $emailInput = $("#email");
+  const $emailError = $("#email-error");
+  const $nameInput = $("#name");
+  const $nameError = $("#name-error");
+  const $messageInput = $("#message");
+  const $messageError = $("#message-error");
+
+  const $modal = $("#successModal");
+  const $closeModalBtn = $("#closeModalBtn");
+
+  const savedData = JSON.parse(sessionStorage.getItem("formData") || "{}");
+
+  if (savedData.name) $nameInput[0].value = savedData.name;
+  if (savedData.email) $emailInput[0].value = savedData.email;
+  if (savedData.message) $messageInput[0].value = savedData.message;
+
+  function showModal() {
+    $modal.addClass("is-visible").attr("aria-hidden", "false");
+  }
+
+  function closeModal() {
+    $modal.removeClass("is-visible").attr("aria-hidden", "true");
+    $form[0].reset(); // Reset form fields after closing
+  }
+
+  function validateEmail() {
+    const validity = $emailInput[0].validity;
+    if (validity.valid) {
+      $emailError.text("").removeClass("active");
+      $emailInput.removeClass("is-invalid");
+      return true;
+    }
+
+    if (validity.valueMissing) {
+      $emailError.text("Please enter your email address.");
+    } else if (validity.typeMismatch) {
+      $emailError.text(
+        "Please enter a valid email address (e.g., name@domain.com).",
+      );
+    }
+
+    $emailError.addClass("active");
+    $emailInput.addClass("is-invalid");
+    return false;
+  }
+
+  function validateName() {
+    const validity = $nameInput[0].validity;
+    if (validity.valid) {
+      $nameError.text("").removeClass("active");
+      $nameInput.removeClass("is-invalid");
+      return true;
+    }
+
+    if (validity.valueMissing) {
+      $nameError.text("Please enter your name.");
+    }
+
+    $nameError.addClass("active");
+    $nameInput.addClass("is-invalid");
+    return false;
+  }
+
+  function validateMessage() {
+    const validity = $messageInput[0].validity;
+    if (validity.valid) {
+      $messageError.text("").removeClass("active");
+      $messageInput.removeClass("is-invalid");
+      return true;
+    }
+
+    if (validity.valueMissing) {
+      $messageError.text("Please enter your message.");
+    }
+
+    $messageError.addClass("active");
+    $messageInput.addClass("is-invalid");
+    return false;
+  }
+
+  function saveInput(e) {
+    const sessionData = JSON.parse(sessionStorage.getItem("formData") || "{}");
+    sessionData[e.target.name] = e.target.value;
+    sessionStorage.setItem("formData", JSON.stringify(sessionData));
+  }
+
+  $emailInput.on("input", function (e) {
+    validateEmail();
+    saveInput(e);
+  });
+
+  $nameInput.on("input", function (e) {
+    validateName();
+    saveInput(e);
+  });
+  $messageInput.on("input", function (e) {
+    validateMessage();
+    saveInput(e);
+  });
+
+  $form.on("submit", function (event) {
+    event.preventDefault();
+
+    const isEmailValid = validateEmail();
+    const isNameValid = validateName();
+    const isMessageValid = validateMessage();
+
+    if (isEmailValid && isNameValid && isMessageValid) {
+      showModal();
+      sessionStorage.removeItem("formData");
+    }
+  });
+
+  $closeModalBtn.on("click", closeModal);
+
+  $modal.on("click", function (e) {
+    if ($(e.target).is("#successModal")) {
+      closeModal();
+    }
+  });
+});
